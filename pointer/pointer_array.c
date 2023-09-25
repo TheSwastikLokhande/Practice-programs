07f8043a94e2a781478095c089533887de2bcdf4
Swastik Lokhande <82387550+TheSwastikLokhande@users.noreply.github.com>
2023-09-25 15:09:45 +0000

    update

--- pointer/pointer_array.c
+++ pointer/pointer_array.c
@@ -1,31 +1,39 @@
-#include <stdio.h>
-int mian()
+int main()
 {
-    float *p[4], dis, tot;
-    int i, cnt[4], j;
+    float *p[4],tot,dis;
+    int i, j, ic[4];
 
-    for (i = 0; i < cnt; i++)
+    for(i=0;i<4;i++)
     {
+        printf("\n Enter the item count of %d buyer: ",i+1);
+        scanf("%d",&ic[i]);
 
-        printf("Enter the items count: ");
-        scanf("%d", &cnt[i]);
-        p[i] = (float *)malloc(cnt[i] * (sizeof(int)));
+        p[i]=(float*)malloc(ic[i]*sizeof(float));
 
-        printf("Enbter the %d th player items price: ");
-        for (j = 0; j < cnt[i]; j++)
+        printf("\n Enter the price of %d items: ",ic[i]);
+        for(j=0;j<ic[i];j++)
         {
-            scanf("%f", (p[i] + j));
+            scanf("%f",*(p+i)+j);
         }
     }
 
     printf("\n List of Items with price \n");
-
-    for (i = 0; i < 4; i++)
+    for(i=0;i<4;i++)
     {
         tot=0.0;
         dis=0.0;
-        printf("buyer %d",i+1);
-        for()
+        printf("\n Buyer %d: ",i+1);
+        for(j=0;j<ic[i];j++)
+        {
+            printf("  %7.2f",*(*(p+i)+j));
+            tot=tot + *(*(p+i)+j);
+        }
+        if(tot<500)
+            dis=tot*0.05;
+        else
+            dis=tot*0.14;
+
+        printf("\t total bill: %0.2f \t Discount: %0.2f \t Pay Rs: %0.2f",tot,dis,(tot-dis));
     }
     return 0;
 }
